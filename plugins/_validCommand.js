560d88422dd33d6471ff60febc5e0dfbbc7fdc45
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-21 14:54:32 -0500

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -16,7 +16,7 @@ export async function before(m, { conn }) {
     'menu', 'bass', 'blown', 'deep', 'earrape', 'fast', 'fat', 'tienda', 'comprar', 
     'nightcore', 'reverse', 'robot', 'slow', 'smooth', 'desblock','ayuda', 'tupai', 'squirrel', 'kick', 'basura', ' echar', 'echar', 'ban', 'rip', 'basura', 
     'setname', 'newnombre','nuevonombre',
-'setpp', 'group', 'grup', 'gc', 
+'setpp', 'group', 'grup', 'gc','todos', 
 
 
 
