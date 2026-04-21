82e6abdae8abf74b2dd5693f02f2926352805b8d
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-21 12:29:55 -0500

    Update chatbot.js

--- plugins/Grupos/chatbot.js
+++ plugins/Grupos/chatbot.js
@@ -3,19 +3,13 @@ export async function before(m, { conn }) {
   if (!m.text) return
   if (m.isBaileys) return
 
-  // Detecta exactamente "bot" sin prefijo
   if (m.text.trim().toLowerCase() === 'bot') {
-
-    let user = '@' + m.sender.split('@')[0]
 
     await conn.reply(
       m.chat,
-      `👋 Hola ${user} ¿en qué te puedo ayudar?`,
+      'Hola brow estoy activo ☑️',
       m,
-      {
-        mentions: [m.sender],
-        ...rcanal
-      }
+      rcanal
     )
 
   }
