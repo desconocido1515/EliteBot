b5fba5a353e642b3b259e2c84d45ba8f66b8c2c4
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 01:00:29 -0500

    Update downloads-igfb.js

--- plugins/downloads-igfb.js
+++ plugins/downloads-igfb.js
@@ -1,6 +1,6 @@
 const handler = async (m, { args, conn, usedPrefix }) => {
 try {
-if (!args[0]) return conn.reply(m.chat, `❀ Por favor, ingresa un enlace de Instagram/Facebook.`, m)
+if (!args[0]) return conn.reply(m.chat, `☑️ 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁, 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼/𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺`, m, rcanal)
 let data = []
 try {
 await m.react('🕒')
@@ -19,13 +19,13 @@ if (json.status && json.data?.length) {
 data = json.data.map(v => v.url)
 }} catch {}
 }
-if (!data.length) return conn.reply(m.chat, `ꕥ No se pudo obtener el contenido.`, m)
+if (!data.length) return conn.reply(m.chat, `☑️ 𝙽𝙾 𝚂𝙴 𝙿𝚄𝙳𝙾 𝙾𝙱𝚃𝙴𝙽𝙴𝚁 𝙴𝙻 𝙲𝙾𝙽𝚃𝙴𝙽𝙸𝙳𝙾`, m, rcanal)
 for (let media of data) {
-await conn.sendFile(m.chat, media, 'instagram.mp4', `> 🍙 Aquí tienes ฅ^•ﻌ•^ฅ.`, m)
+await conn.sendFile(m.chat, media, 'instagram.mp4', `☑️ 𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂 𝚃𝚄 𝙲𝙾𝙽𝚃𝙴𝙽𝙸𝙳𝙾 🍙`, m)
 await m.react('✔️')
 }} catch (error) {
 await m.react('✖️')
-await m.reply(`⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`)
+await conn.reply(m.chat, `☑️ 𝚂𝙴 𝙷𝙰 𝙿𝚁𝙾𝙳𝚄𝙲𝙸𝙳𝙾 𝚄𝙽 𝙿𝚁𝙾𝙱𝙻𝙴𝙼𝙰\n\n𝙴𝚁𝚁𝙾𝚁: ${error.message}`, m, rcanal)
 }}
 
 handler.command = ['instagram', 'ig', 'facebook', 'fb']
