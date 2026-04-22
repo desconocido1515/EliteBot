45a16a8d5680367dacfdb49029972c6167a99262
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-21 23:09:18 -0500

    Update Podio.js

--- plugins/Free fire/Podio.js
+++ plugins/Free fire/Podio.js
@@ -1,12 +1,36 @@
-var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = podiocuadri.getRandom()
-const cat = 
-`» 𝙋𝙊𝘿𝙄𝙊 𝘾𝙐𝘼𝘿𝙍𝙄𝙇𝘼́𝙏𝙀𝙍𝙊 🏆`
-
-await conn.sendFile(m.chat, pp, 'img5.jpg', cat, fkontak)
+var handler = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+    let fkontak = { 
+        "key": { 
+            "participants": "0@s.whatsapp.net", 
+            "remoteJid": "status@broadcast", 
+            "fromMe": false, 
+            "id": "Halo" 
+        }, 
+        "message": { 
+            "contactMessage": { 
+                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        "participant": "0@s.whatsapp.net" 
+    }
+    
+    // Reaccionar al mensaje
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '🏆',
+            key: m.key
+        }
+    })
+    
+    // Ruta de la imagen
+    const pp = './media/menus/podiocuadri.jpg'
+    
+    const cat = `» 𝙋𝙊𝘿𝙄𝙊 𝘾𝙐𝘼𝘿𝙍𝙄𝙇𝘼́𝙏𝙀𝙍𝙊 🏆`
+    
+    await conn.sendFile(m.chat, pp, 'podiocuadri.jpg', cat, fkontak)
 }
-handler.help = ['owner', 'creator']
+
+handler.help = ['podiocuadri']
 handler.tags = ['info']
 handler.command = /^(podiocuadri)$/i
 
