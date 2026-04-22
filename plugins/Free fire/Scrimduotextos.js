977c907746103ef7810df7779edac7072fa620fe
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 00:15:55 -0500

    Update Scrimduotextos.js

--- plugins/Free fire/Scrimduotextos.js
+++ plugins/Free fire/Scrimduotextos.js
@@ -1,31 +1,29 @@
 let handler = async(m, { user, isOwner, isAdmin, conn, text, participants, args, command }) => {
-if (!(isAdmin || isOwner || user)) {
-global.dfail('admin', m, conn)
-throw false
+    if (!(isAdmin || isOwner || user)) {
+        return conn.reply(m.chat, `☑️ 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂 𝚂𝙴𝚁 𝙰𝙳𝙼𝙸𝙽 𝙿𝙰𝚁𝙰 𝚄𝚂𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾`, m, rcanal)
+    }
+    
+    let pesan = args.join` `
+    let oi = `𝗦𝗖𝗥𝗜𝗠 𝗗𝗘 𝗗𝗨𝗢𝗦 📍 ${pesan}`
+    let teks = `${oi}\n`
+    teks += `⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨\n\n`
+    teks += `» 𝐃𝐢𝐚: \n`
+    teks += `» 𝐇𝐨𝐫𝐚: \n\n`
+    teks += `» *𝐏𝐋𝐀𝐘𝐄𝐑𝐒*\n\n`
+    teks += `♥️ 𝗗𝗨𝗢 *1*\n`
+    teks += `🦄➛\n`
+    teks += `🦄➛\n\n`
+    teks += `♥️ 𝗗𝗨𝗢 *2*\n`
+    teks += `🌈➛\n`
+    teks += `🌈➛\n`
+    
+    await conn.reply(m.chat, teks, m, rcanal)
 }
-let pesan = args.join` `
-let oi = `𝗦𝗖𝗥𝗜𝗠 𝗗𝗘 𝗗𝗨𝗢𝗦 📍 ${pesan}`
-let teks = `${oi}\n`
-teks += `⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨
 
-» 𝐃𝐢𝐚: 
-» 𝐇𝐨𝐫𝐚: 
-
-» *𝐏𝐋𝐀𝐘𝐄𝐑𝐒*
-
-♥️ 𝗗𝗨𝗢 *1*
-🦄➛
-🦄➛
-
-♥️ 𝗗𝗨𝗢 *2*
-🌈➛
-🌈➛
-`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-}
-handler.help = ['smsf20 <mesaje>','fem20 <mesaje>']
+handler.help = ['scrimduos', 'scrimduo']
 handler.tags = ['group']
 handler.command = /^(scrimduos|scrimduo)$/i
-handler.admin = false
 handler.group = true
+handler.admin = true
+
 export default handler
