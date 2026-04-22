26656a19cc2d6bbd4f916e3a361ca0e73f6832fa
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-21 22:40:10 -0500

    Update Agenda.js

--- plugins/Free fire/Agenda.js
+++ plugins/Free fire/Agenda.js
@@ -1,12 +1,36 @@
-var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = agenda.getRandom()
-const cat = 
-`» 𝘼𝙂𝙀𝙉𝘿𝘼 𝘿𝙀 𝙑𝙀𝙍𝙎𝙐𝙎 🔰`
-
-await conn.sendFile(m.chat, pp, 'img5.jpg', cat, fkontak)
+var handler = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+    let fkontak = { 
+        "key": { 
+            "participants": "0@s.whatsapp.net", 
+            "remoteJid": "status@broadcast", 
+            "fromMe": false, 
+            "id": "Halo" 
+        }, 
+        "message": { 
+            "contactMessage": { 
+                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        "participant": "0@s.whatsapp.net" 
+    }
+    
+    // Reaccionar al mensaje
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '📋',
+            key: m.key
+        }
+    })
+    
+    // Ruta de la imagen según el comando
+    const pp = `./media/menus/${command}.jpg`
+    
+    const cat = `» 𝘼𝙂𝙀𝙉𝘿𝘼 𝘿𝙀 𝙑𝙀𝙍𝙎𝙐𝙎 🔰`
+    
+    await conn.sendFile(m.chat, pp, `${command}.jpg`, cat, fkontak)
 }
-handler.help = ['owner', 'creator']
+
+handler.help = ['agenda']
 handler.tags = ['info']
 handler.command = /^(agenda)$/i
 
