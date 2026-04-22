07d1dc6bfcfb5e15a3a128dbd5b831c2671c0ef0
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 18:45:10 -0500

    Update Menu.hot.js

--- plugins/Menus/Menu.hot.js
+++ plugins/Menus/Menu.hot.js
@@ -1,21 +1,37 @@
 import fs, { promises } from 'fs'
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '⚠ 𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 +18 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙀𝙉 𝙀𝙎𝙏𝙀 𝙂𝙍𝙐𝙋𝙊, 𝙎𝙄 𝙀𝙎 𝘼𝘿𝙈𝙄𝙉 𝙔 𝘿𝙀𝙎𝙀𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍𝙇𝙊𝙎, 𝙐𝙎𝙀 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 .on modohorny'; 
-try {
-let d = new Date(new Date + 3600000)
-let locale = 'es'
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime)
-let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-let more = String.fromCharCode(8206)
-let readMore = more.repeat(850)   
-let taguser = conn.getName(m.sender)
-let user = global.db.data.users[m.sender]
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let menu = `
+  // Eliminada la restricción de modohorny
+  
+  try {
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+    let more = String.fromCharCode(8206)
+    let readMore = more.repeat(850)   
+    let taguser = conn.getName(m.sender)
+    let user = global.db.data.users[m.sender]
+    let fkontak = { 
+      "key": { 
+        "participants": "0@s.whatsapp.net", 
+        "remoteJid": "status@broadcast", 
+        "fromMe": false, 
+        "id": "Halo" 
+      }, 
+      "message": { 
+        "contactMessage": { 
+          "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+        }
+      }, 
+      "participant": "0@s.whatsapp.net" 
+    }
+    
+    let menu = `
 ¡Hola! 👋🏻 @${m.sender.split("@")[0]}
  \`\`\`${week}, ${date}\`\`\`
 
@@ -60,37 +76,42 @@ let menu = `
 │🔞➺ .𝘩𝘦𝘯𝘵𝘢𝘪
 │🔞➺ .𝘵𝘳𝘢𝘱𝘪𝘵𝘰
 ╰━━━━━━⋆★⋆━━━━━━⬣
- `.trim()
+    `.trim()
     
-const vi = ['https://telegra.ph/file/aa3e11b1cc4246ad72b9b.mp4']
+    const vi = ['https://telegra.ph/file/aa3e11b1cc4246ad72b9b.mp4']
 
-try {
-await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try {
-await conn.sendMessage(m.chat, { image: { url: gataMenu.getRandom() }, gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try {
-await conn.sendMessage(m.chat, { image: gataImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
-} catch (error) {
-try{
-await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
-} catch (error) {
-return 
-}}}} 
-
-} catch (e) {
-await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)}}
+    try {
+      await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+    } catch (error) {
+      try {
+        await conn.sendMessage(m.chat, { image: { url: gataMenu.getRandom() }, gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+      } catch (error) {
+        try {
+          await conn.sendMessage(m.chat, { image: gataImg.getRandom(), gifPlayback: false, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
+        } catch (error) {
+          try {
+            await conn.sendFile(m.chat, imagen5, 'menu.jpg', menu, fkontak, false, { mentions: [m.sender, global.conn.user.jid] })
+          } catch (error) {
+            return 
+          }
+        }
+      }
+    } 
+  } catch (e) {
+    await m.reply(`☑️ Ocurrió un error al mostrar el menú.`)
+    console.log(`❗❗ Error en menú hot: ${e}`)
+  }
+}
 
 handler.command = /^(menuhot)$/i
 handler.register = false
 handler.group = true
+
 export default handler
-    
+
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
