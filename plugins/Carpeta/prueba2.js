c87f30dd3bbdf52436d59dc06af77d8ebfa3295e
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 10:07:03 -0500

    Update prueba2.js

--- plugins/Carpeta/prueba2.js
+++ plugins/Carpeta/prueba2.js
@@ -4,17 +4,17 @@ const handler = async (m, { conn, args }) => {
   const texto = args.join(' ').trim();
 
   if (!texto) {
-    return conn.reply(m.chat, `✦ ¡Hey!\nIngresa las dos palabras separadas por *|*\n\n📌 *Ejemplo:*\n.logodeadpool Kevv|Elite`, m, rcanal);
+    return conn.reply(m.chat, `✦ ¡Hey!\nIngresa las dos palabras separadas por *espacio*\n\n📌 *Ejemplo:*\n.logodeadpool Kevv Elite`, m, rcanal);
   }
 
-  // Separar las dos palabras
-  const palabras = texto.split('|');
+  // Separar las palabras por espacio
+  const palabras = texto.split(' ');
   if (palabras.length < 2) {
-    return conn.reply(m.chat, `✦ ¡Hey!\nDebes ingresar *DOS palabras* separadas por *|*\n\n📌 *Ejemplo:*\n.logodeadpool Kevv|Elite`, m, rcanal);
+    return conn.reply(m.chat, `✦ ¡Hey!\nDebes ingresar *DOS palabras* separadas por *espacio*\n\n📌 *Ejemplo:*\n.logodeadpool Kevv Elite`, m, rcanal);
   }
 
   const textoPequeño = palabras[0].trim();
-  const textoGrande = palabras[1].trim();
+  const textoGrande = palabras.slice(1).join(' ').trim();
 
   try {
     await conn.sendMessage(m.chat, {
