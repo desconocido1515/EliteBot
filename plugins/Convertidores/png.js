b1b608ca4dedcb33e2a6f323f96797991f4f951c
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-21 19:25:46 -0500

    Update png.js

--- plugins/Convertidores/png.js
+++ plugins/Convertidores/png.js
@@ -1,23 +1,24 @@
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn }) => {
     const q = m.quoted || m
 
     let mime = (q.msg || q).mimetype || ''
 
-    if (!m.quoted || !/image/.test(mime))
-        throw `Menciona una foto para convertir a documento PNG.`
-
-    if (!text) throw `Ingrese el nombre que desea colocar al documento`
+    if (!m.quoted || !/image/.test(mime)) {
+        return conn.reply(m.chat, `☑️ 𝙼𝙴𝙽𝙲𝙸𝙾𝙽𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙰 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝙾 𝙿𝙽𝙶`, m, rcanal)
+    }
 
     let media = await q.download?.()
 
-    if (!media) throw 'Error al descargar medio'
+    if (!media) {
+        return conn.reply(m.chat, `☑️ 𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽`, m, rcanal)
+    }
 
-    m.reply(`Convirtiendo a PNG, espera un momento...`)
+    await conn.reply(m.chat, `☑️ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙴𝙽𝙳𝙾 𝙰 𝙿𝙽𝙶...`, m, rcanal)
 
-    return conn.sendMessage(m.chat, { document: media, mimetype: 'image/png', fileName: `${text}.png` }, { quoted: m })
+    return conn.sendMessage(m.chat, { document: media, mimetype: 'image/png', fileName: `Imagen.png` }, { quoted: m })
 }
 
-handler.help = ['png <nombre>']
+handler.help = ['png']
 handler.tags = ['tools']
 handler.command = ['png']
 
