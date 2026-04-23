2a8b371c98654ce4032a4a025781ee916bb1bd30
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 20:24:24 -0500

    Update Adulto4.js

--- plugins/Pornhub/Adulto4.js
+++ plugins/Pornhub/Adulto4.js
@@ -1,45 +1,68 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-const handler = async (m, {conn, text, __dirname, usedPrefix, command}) => {
-  //if (!global.db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
-  if (!text) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝙷𝙴𝙽𝚃𝙰𝙸 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*';
-  const searchResults = await searchHentai(text);
-  let teks = searchResults.result.map((v, i) => `
-${i+1}. *_${v.title}_*
+
+const handler = async (m, { conn, text = '', command }) => {
+
+  // 🔥 Validación correcta
+  if (!text || text.trim() === '') {
+    return conn.reply(
+      m.chat,
+      '*[❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝘼𝙇𝙂𝙐𝙉 𝙃𝙀𝙉𝙏𝘼𝙄 𝘼 𝘽𝙐𝙎𝘾𝘼𝙍*',
+      m
+    );
+  }
+
+  try {
+    const searchResults = await searchHentai(text);
+
+    let teks = searchResults.result.map((v, i) => `
+${i + 1}. *_${v.title}_*
 ↳ 📺 *_Vistas:_* ${v.views}
 ↳ 🎞️ *_Link:_* ${v.url}`).join('\n\n');
-  let randomThumbnail;
-  if (searchResults.result.length > 0) {
-    const randomIndex = Math.floor(Math.random() * searchResults.result.length);
-    randomThumbnail = searchResults.result[randomIndex].thumbnail;
-  } else {
-    randomThumbnail = 'https://pictures.hentai-foundry.com/e/Error-Dot/577798/Error-Dot-577798-Zero_Two.png';
-    teks = '*[❗] 𝙽𝙾 𝚂𝙴 𝙷𝙰𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙾 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*';
+
+    let randomThumbnail;
+
+    if (searchResults.result.length > 0) {
+      const randomIndex = Math.floor(Math.random() * searchResults.result.length);
+      randomThumbnail = searchResults.result[randomIndex].thumbnail;
+    } else {
+      randomThumbnail = 'https://pictures.hentai-foundry.com/e/Error-Dot/577798/Error-Dot-577798-Zero_Two.png';
+      teks = '*[❗] 𝙽𝙾 𝚂𝙴 𝙷𝙰𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙾 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂*';
+    }
+
+    await conn.sendFile(m.chat, randomThumbnail, 'result.jpg', teks, m);
+
+  } catch (e) {
+    console.error(e);
+    conn.reply(m.chat, '*[❗] Error al buscar resultados*', m);
   }
-  conn.sendFile(m.chat, randomThumbnail, 'error.jpg', teks, m);
 };
+
 handler.command = /^(hentaisearch|searchhentai)$/i;
 export default handler;
+
+
+// 🔍 Función de búsqueda
 async function searchHentai(search) {
-  return new Promise((resolve, reject) => {
-    axios.get('https://hentai.tv/?s=' + search).then(async ({data}) => {
-      const $ = cheerio.load(data);
-      const result = {};
-      const res = [];
-      result.coder = 'rem-comp';
-      result.result = res;
-      result.warning = 'It is strictly forbidden to reupload this code, copyright © 2022 by rem-comp';
-      $('div.flex > div.crsl-slde').each(function(a, b) {
-        const _thumbnail = $(b).find('img').attr('src');
-        const _title = $(b).find('a').text().trim();
-        const _views = $(b).find('p').text().trim();
-        const _url = $(b).find('a').attr('href');
-        const hasil = {thumbnail: _thumbnail, title: _title, views: _views, url: _url};
-        res.push(hasil);
-      });
-      resolve(result);
-    }).catch((err) => {
-      console.log(err);
+  try {
+    const { data } = await axios.get('https://hentai.tv/?s=' + encodeURIComponent(search));
+    const $ = cheerio.load(data);
+
+    const result = [];
+    
+    $('div.flex > div.crsl-slde').each((a, b) => {
+      const thumbnail = $(b).find('img').attr('src');
+      const title = $(b).find('a').text().trim();
+      const views = $(b).find('p').text().trim();
+      const url = $(b).find('a').attr('href');
+
+      result.push({ thumbnail, title, views, url });
     });
-  });
+
+    return { result };
+
+  } catch (err) {
+    console.error(err);
+    return { result: [] };
+  }
 }
