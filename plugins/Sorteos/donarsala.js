2fdfdd0a9519096a1642e41e84e79ce2f0c61d16
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 21:05:15 -0500

    Update donarsala.js

--- plugins/Sorteos/donarsala.js
+++ plugins/Sorteos/donarsala.js
@@ -1,15 +1,18 @@
 let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)},* _Te toco donar una sala mi pana juegas y juegas y no te he visto colaborar una sala._`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
+
+function handler(m, { groupMetadata, conn }) {
+  let ps = groupMetadata.participants.map(v => v.id)
+  let a = ps.getRandom()
+  let b
+  do b = ps.getRandom()
+  while (b === a)
+  
+  conn.reply(m.chat, `*${toM(a)},* _Te toco donar una sala mi pana juegas y juegas y no te he visto colaborar una sala._`, m, rcanal)
+}
+
+handler.help = ['donarsala']
 handler.tags = ['main', 'fun']
 handler.command = ['donarsala']
 handler.group = true
+
 export default handler
