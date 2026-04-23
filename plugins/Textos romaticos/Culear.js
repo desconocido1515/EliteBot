598ebf2ef6bf3fdc1984f81224d37952fc85345d
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 22:38:12 -0500

    Update Culear.js

--- plugins/Textos romaticos/Culear.js
+++ plugins/Textos romaticos/Culear.js
@@ -1,20 +1,25 @@
 var handler = async (m, { conn, command, text }) => {
-
-if (!text) throw `🍭 𝙀𝙎𝘾𝙍𝙄𝘽𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝘿𝙊𝙎 𝙋𝙀𝙍𝙎𝙊𝙉𝘼𝙎 𝙊 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙇𝙊𝙎 𝙋𝘼𝙍𝘼 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙍 𝙎𝙄 𝙎𝙀 𝙋𝙐𝙀𝘿𝙀𝙉 𝘾𝙐𝙇𝙀𝘼𝙍.`
-let [text1, ...text2] = text.split(' ')
-
-text2 = (text2 || []).join(' ')
-if (!text2) throw `🔥 𝙀𝙎𝘾𝙍𝙄𝘽𝙀 𝙊 𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙇𝘼 𝙎𝙀𝙂𝙐𝙉𝘿𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼.`
-let love = `━━━━━━━━━━━━━━━
+  if (!text) {
+    return conn.reply(m.chat, `🍭 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙳𝙾𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝙻𝙾𝚂 𝙿𝙰𝚁𝙰 𝙲𝙰𝙻𝙲𝚄𝙻𝙰𝚁 𝚂𝙸 𝚂𝙴 𝙿𝚄𝙴𝙳𝙴𝙽 𝙲𝚄𝙻𝙴𝙰𝚁.`, m, rcanal);
+  }
+  
+  let [text1, ...text2] = text.split(' ');
+  text2 = (text2 || []).join(' ');
+  
+  if (!text2) {
+    return conn.reply(m.chat, `🔥 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝚂𝙴𝙶𝚄𝙽𝙳𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰.`, m, rcanal);
+  }
+  
+  let love = `━━━━━━━━━━━━━━━
 😈 *${text1}* 𝙏𝙐 𝙊𝙋𝙊𝙍𝙏𝙐𝙉𝙄𝘿𝘼𝘿 𝘿𝙀 𝘾𝙐𝙇𝙀𝘼𝙍𝙏𝙀 𝘼 *${text2}* 𝙀𝙎 𝘿𝙀 *${Math.floor(Math.random() * 100)}%* 🔥 
 ━━━━━━━━━━━━━━━
-𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇 
-`.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })
-
+𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇`.trim();
+  
+  m.reply(love, null, { mentions: conn.parseMention(love) });
 }
+
 handler.help = ['love']
 handler.tags = ['fun']
 handler.command = /^(culear|culiar)$/i
 
-export default handler
+export default handler;
