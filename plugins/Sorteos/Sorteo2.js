2e31e469bb6a382bf543b470ada12d784bc4e5e2
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-22 20:36:53 -0500

    Update Sorteo2.js

--- plugins/Sorteos/Sorteo2.js
+++ plugins/Sorteos/Sorteo2.js
@@ -3,17 +3,23 @@
 -----------------------------------------------------------------------------------------*/
 import util from 'util'
 import path from 'path'
+
 let user = a => '@' + a.split('@')[0]
+
 async function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `𝙀𝙟𝙚𝙢𝙥𝙡𝙤 𝙙𝙚 𝙪𝙨𝙤:\n.sorteo2 𝙩𝙚𝙭𝙩𝙤`
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['ㅤ'])}`
-let l = Math.floor(Math.random() * x.length);
-let vn = ``
-let top = `*${x}🎊━━━𝙎𝙊𝙍𝙏𝙀𝙊━━━🎊*
+  if (!text) {
+    return conn.reply(m.chat, `☑️ 𝙀𝙟𝙚𝙢𝙥𝙡𝙤 𝙙𝙚 𝙪𝙨𝙤:\n.sorteo2 𝙩𝙚𝙭𝙩𝙤`, m, rcanal)
+  }
+  
+  let ps = groupMetadata.participants.map(v => v.id)
+  let a = ps.getRandom()
+  let b = ps.getRandom()
+  let k = Math.floor(Math.random() * 70);
+  let x = `${pickRandom(['ㅤ'])}`
+  let l = Math.floor(Math.random() * x.length);
+  let vn = ``
+  
+  let top = `*${x}🎊━━━𝙎𝙊𝙍𝙏𝙀𝙊━━━🎊*
 
 📩 𝙈𝙀𝙉𝙎𝘼𝙅𝙀: 
 *${text} ${x}*
@@ -25,26 +31,42 @@ let top = `*${x}🎊━━━𝙎𝙊𝙍𝙏𝙀𝙊━━━🎊*
 ⇝𝙏𝙄𝙀𝙉𝙀𝙉 𝙋𝙇𝘼𝙕𝙊 𝘿𝙀 𝘾𝙊𝙉𝙁𝙄𝙍𝙈𝘼𝙍𝙈𝙀 𝙀𝙉 𝙋𝙍𝙄𝙑𝘼𝘿𝙊, 𝙀𝙉 𝘾𝘼𝙎𝙊 𝘾𝙊𝙉𝙏𝙍𝘼𝙍𝙄𝙊́ 𝙀𝙎𝙏𝘼𝙉 𝙁𝙐𝙀𝙍𝘼 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊.
 *_________________*
 `
-let txt = '';
-let count = 0;
-for (const c of top) {
+  
+  let txt = '';
+  let count = 0;
+  
+  for (const c of top) {
     await new Promise(resolve => setTimeout(resolve, 15));
     txt += c;
     count++;
 
     if (count % 10 === 0) {
-        conn.sendPresenceUpdate('composing' , m.chat);
+      conn.sendPresenceUpdate('composing', m.chat);
     }
+  }
+  
+  await conn.sendMessage(m.chat, { 
+    text: txt.trim(), 
+    mentions: conn.parseMention(txt) 
+  }, { 
+    quoted: m, 
+    ephemeralExpiration: 24*60*100, 
+    disappearingMessagesInChat: 24*60*100
+  });
+  
+  conn.sendFile(m.chat, vn, 'audio.opus', null, m, true, {
+    type: 'audioMessage',
+    ptt: true
+  })
 }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} );
-//m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
-conn.sendFile(m.chat, vn, '🏆ACERQUESE A RECLAMAR SU PREMIO🏅', null, m, true, {
-type: 'audioMessage',
-ptt: true })}
+
 handler.help = handler.command = ['sorteo2']
 handler.tags = ['fun']
 handler.group = true
 handler.limit = 0
+
 export default handler
+
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)]
+}
