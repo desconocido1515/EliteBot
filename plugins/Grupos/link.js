dbfb03bf8b65385f7728f0c7f4b5217364a4107e
desconocido <129682184+desconocido1515@users.noreply.github.com>
2026-04-24 22:37:36 -0500

    Create link.js

---
+++ plugins/Grupos/link.js
@@ -0,0 +1,16 @@
+var handler = async (m, { conn, args }) => {
+    let group = m.chat
+    let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+    let message = `☑️ *LINK DEL GRUPO* ☑️\n\n📌 *Enlace:*\n${link}\n\n⚠️ *Comparte este enlace para invitar nuevos miembros.*`
+    
+    await conn.reply(m.chat, message, m, rcanal)
+}
+
+handler.help = ['link']
+handler.tags = ['group']
+  
+handler.command = ['link', 'invite', 'invitacion']
+handler.group = true
+handler.botAdmin = true
+
+export default handler
